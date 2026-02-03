b41beb247bec4c752cbc6ff5558b873f7ac232c8
Guruvigneshv45 <141053632+Guruvigneshv45@users.noreply.github.com>
2026-02-03 23:42:41 +0530

    Uploaded Answers for Assignment 4

--- embedUR_Linux_Mod_4/file_manip2.sh
+++ embedUR_Linux_Mod_4/file_manip2.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-awk '
-index($0,"frame.time") ||
-index($0,"wlan.fc.type") ||
-index($0,"wlan.fc.subtype")
-' "$1"
+awk 'index($0,"frame.time") || index($0,"wlan.fc.type") || index($0,"wlan.fc.subtype")' "$1"
 
